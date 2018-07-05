271cbd54de32e373964b0359ca2e3789ea26657c
Vitaly Dyatlov <md.xytop@gmail.com>
2018-07-05 14:41:06 +0000

    update headers to 10.6

--- nodejs/make.bat
+++ nodejs/make.bat
@@ -6,8 +6,8 @@ curl https://nodejs.org/dist/v8.1.2/node-v8.1.2-headers.tar.gz | tar xz -C targe
 curl https://nodejs.org/dist/v8.1.2/win-x64/node.lib > targets/node-v8.1.2/node.lib
 curl https://nodejs.org/dist/v9.2.0/node-v9.2.0-headers.tar.gz | tar xz -C targets
 curl https://nodejs.org/dist/v9.2.0/win-x64/node.lib > targets/node-v9.2.0/node.lib
-curl https://nodejs.org/dist/v10.0.0/node-v10.0.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v10.0.0/win-x64/node.lib > targets/node-v10.0.0/node.lib
+curl https://nodejs.org/dist/v10.6.0/node-v10.6.0-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/v10.6.0/win-x64/node.lib > targets/node-v10.6.0/node.lib
 )
 
 cp README.md dist/README.md
@@ -20,7 +20,7 @@ cp src/uws.js dist/uws.js
 
 cl /I targets/node-v8.1.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.1.2/node.lib
 cl /I targets/node-v9.2.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.2.0/node.lib
-cl /I targets/node-v10.0.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.0.0/node.lib
+cl /I targets/node-v10.6.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.6.0/node.lib
 
 rm *.obj
 rm dist/*.exp
