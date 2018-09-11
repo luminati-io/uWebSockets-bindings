a0c0fcd8a22f9079fcadc80d4e45ba09690644d5
Sir Anthony <anthony@adsorbtion.org>
2018-09-11 11:21:26 +0300

    Use latest node, improve make.bat, fix windows build

--- nodejs/make.bat
+++ nodejs/make.bat
@@ -1,13 +1,22 @@
-call "%VS140COMNTOOLS%..\..\vc\vcvarsall.bat" amd64
+set vspath="%VS140COMNTOOLS%..\"
+if defined GYP_MSVS_OVERRIDE_PATH (
+  set vspath=%GYP_MSVS_OVERRIDE_PATH%
+)
+
+call "%vspath%..\vc\vcvarsall.bat" amd64
+
+set v57=v8.1.2
+set v59=v9.2.0
+set v64=v10.10.0
 
 if not exist targets (
 mkdir targets
-curl https://nodejs.org/dist/v8.1.2/node-v8.1.2-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v8.1.2/win-x64/node.lib > targets/node-v8.1.2/node.lib
-curl https://nodejs.org/dist/v9.2.0/node-v9.2.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v9.2.0/win-x64/node.lib > targets/node-v9.2.0/node.lib
-curl https://nodejs.org/dist/v10.6.0/node-v10.6.0-headers.tar.gz | tar xz -C targets
-curl https://nodejs.org/dist/v10.6.0/win-x64/node.lib > targets/node-v10.6.0/node.lib
+curl https://nodejs.org/dist/%v57%/node-%v57%-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/%v57%/win-x64/node.lib > targets/node-v8.1.2/node.lib
+curl https://nodejs.org/dist/%v59%/node-%v59%-headers.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/%v59%/win-x64/node.lib > targets/node-v9.2.0/node.lib
+curl https://nodejs.org/dist/%v64%/node-%v64%.tar.gz | tar xz -C targets
+curl https://nodejs.org/dist/%v64%/win-x64/node.lib > targets/node-v10.10.0/node.lib
 )
 
 cp README.md dist/README.md
@@ -15,12 +24,11 @@ cp ../uWebSockets/LICENSE dist/LICENSE
 cp -r ../uWebSockets/src dist/
 cp src/addon.cpp dist/src/addon.cpp
 cp src/addon.h dist/src/addon.h
-cp src/http.h dist/src/http.h
 cp src/uws.js dist/uws.js
 
-cl /I targets/node-v8.1.2/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-v8.1.2/node.lib
-cl /I targets/node-v9.2.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-v9.2.0/node.lib
-cl /I targets/node-v10.6.0/include/node /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-v10.6.0/node.lib
+cl /I targets/node-%v57%/include/node /EHsc /Ox /LD /Fedist/uws_win32_57.node dist/src/*.cpp targets/node-%v57%/node.lib
+cl /I targets/node-%v59%/include/node /EHsc /Ox /LD /Fedist/uws_win32_59.node dist/src/*.cpp targets/node-%v59%/node.lib
+cl /I targets/node-%v64%/src /I targets/node-%v64%/deps/uv/include /I targets/node-%v64%/deps/v8/include /I targets/node-%v64%/deps/openssl/openssl/include /I targets/node-%v64%/deps/zlib /EHsc /Ox /LD /Fedist/uws_win32_64.node dist/src/*.cpp targets/node-%v64%/node.lib
 
 rm *.obj
 rm dist/*.exp
--- nodejs/src/addon.h
+++ nodejs/src/addon.h
@@ -17,6 +17,26 @@ class TLSWrapSSLGetter : public node::TLSWrap {
 public:
     SSL* getSSL(){ return this->ssl_.get(); }
 };
+
+#if defined(_MSC_VER)
+NO_RETURN void node::Assert(const char* const (*args)[4]) {
+  auto filename = (*args)[0];
+  auto linenum = (*args)[1];
+  auto message = (*args)[2];
+  auto function = (*args)[3];
+
+  char name[1024];
+  char title[1024] = "Node.js";
+  uv_get_process_title(title, sizeof(title));
+  snprintf(name, sizeof(name), "%s[%d]", title, uv_os_getpid());
+
+  fprintf(stderr, "%s: %s:%s:%s%s Assertion `%s' failed.\n",
+          name, filename, linenum, function, *function ? ":" : "", message);
+  fflush(stderr);
+  ABORT_NO_BACKTRACE();
+}
+#endif
+
 #undef NODE_WANT_INTERNALS
 #endif
 
